86956d67def89e0f4f2c7a22bcac6737fb738ad0
Henry <ebukahenry146@gmail.com>
2025-10-10 23:06:06 +0100

    message

--- week_2/assignment.py
+++ week_2/assignment.py
@@ -55,47 +55,64 @@ print(book)
 # 6. Create a list of 5 countries and print the first and last country.
 # Answer no 6 here.
 
-list = ["Nigeria", "Australiar", "Zimbabwe", "Russia", "USA"]
+list = ["Nigeria", "Australia", "Zimbabwe", "Russia", "USA"]
 print(list[0])
 print(list[4])
 
 # 7. Replace the 2nd item in your list of countries with "Canada".
 # Answer no 7 here.
 
-list.insert[1,"Canada"]
+list.insert(1,"Canada")
 print(list)
 
 # 8. Delete the 3rd item in your list of countries.
 # Answer no 8 here.
 
+del list[2]
+print(list)
 
 # 9. Concatenate [10, 20, 30] with [40, 50, 60] and print the new list.
 # Answer no 9 here.
 
+concat = [10, 20, 30] + [40, 50, 60]
+print(concat)
 
 # 10. Check if "Kenya" exists inside your list of countries and print the result.
 # Answer no 10 here.
 
+is_in_list = "Kenya " in list
+print(is_in_list)
 
 # 11. Create a list with the word "Hello" repeated 4 times.
 # Answer no 11 here.
 
+nenye = ["Hello"] * 4
+print(nenye)
 
 # 12. Use append() to add "Japan" to your list of countries.
 # Answer no 12 here.
 
+list.append("Japan")
+print(list)
 
 # 13. Use insert() to add "Brazil" at position 2 in your list of countries.
 # Answer no 13 here.
 
+list.insert(3, "Brazil")
+print(list)
 
 # 14. Use pop() to remove the last item from a list of numbers = [100, 200, 300, 400].
 # Answer no 14 here.
 
+ebuka = [100, 200, 300, 400]
+ebuka.pop(3)
+print(ebuka)
 
 # 15. Use remove() to delete the number 200 from the list numbers = [100, 200, 300, 400].
 # Answer no 15 here.
 
+ebuka.remove(200)
+print(ebuka)
 
 # ----------- NESTED LIST -----------
 
@@ -103,6 +120,8 @@ print(list)
 #     - Print the value "b"
 #     - Add "d" into the "letters" list
 # Answer no 16 here.
+
+
 
 
 # ----------- TUPLE -----------
